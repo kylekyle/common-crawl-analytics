1d6b717f75e3be79da2f374a1869d359154f8139
Madeleine Schneider <mmschneider15@gmail.com>
2019-08-05 10:20:22 -0400

    Update SimpleApp.scala
    Proper data read in and Try addition

--- src/main/scala/edu/usma/cc/SimpleApp.scala
+++ src/main/scala/edu/usma/cc/SimpleApp.scala
@@ -1,9 +1,12 @@
 
 package edu.usma.cc
 
+
+
 import scala.io.Source
 import java.io._
 import java.net.URI
+import scala.util.matching.Regex
 
 import com.martinkl.warc.WARCFileReader
 import com.martinkl.warc.WARCFileWriter
@@ -17,108 +20,71 @@ import org.apache.spark._
 import org.apache.spark.sql.SparkSession
 
 import org.apache.hadoop.io._
-import scala.collection.mutable.ArrayBuffer
 import org.apache.spark.sql.functions._
+
 
 object SimpleApp {
 
-
-
   def main(args: Array[String]) {
 
-    val warcPathFirstHalf = "s3://commoncrawl/"
+    var firstDir = "s3://commoncrawl/crawl-data/CC-MAIN-2019-26/segments/*/wet/*"
+      
+      //example of full file path
+      //"s3://commoncrawl/crawl-data/CC-MAIN-2016-18/segments/1461860106452.21/wet/CC-MAIN-20160428161506-*"
+
 
 
     // Initialize the sparkSession
     val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
     val sc = spark.sparkContext
+
     import spark.implicits._
+    // Open the files as an RDD of type [(LongWritable, WARCWritable)]
+    val warcInput = sc.newAPIHadoopFile(firstDir, classOf[WARCInputFormat], classOf[LongWritable],classOf[WARCWritable]) 
+       
+    // Isolate only the WARCWritables in this RDD
+    var firstWARCs = warcInput.values
 
-    
-    //Load in list of all commoncrawl paths 
-    val source = sc.textFile("s3://eecs-practice/spark-test/wet2018.paths")
-    
-    //get number of paths
-    val length = source.count().toInt
-    //put it into an Array in order to loop, use a smaller number than length to work on less files
-    val lineArray = source.take(length)
 
-    //build up RDD of all records
-    val finalRDD = sc.union(lineArray.map(newPath => sc.newAPIHadoopFile(warcPathFirstHalf + newPath, classOf[WARCInputFormat], classOf[LongWritable],classOf[WARCWritable]).values))
-   
-    //find emails and convert to dataframe
-    val newDF = finalRDD.flatMap( warc => analyze2(warc.getRecord)).filter(tup => tup._2 != null).toDF("email","url")
+    val milEmailPattern = new Regex("""\b[A-Za-z0-9._%+-]{1,64}@.ic.gov\b""")
+
+    // pull out the ic.gov email 
+    val newRDD = firstWARCs.flatMap( warc => 
+      Try{
+      analyze(warc.getRecord)
+    }.toOption)
+    
+    //convert to DF, it's easier to do analytics on this datastructure 
+    val newDF = newRDD.toDF("email","url")
     val reducedDF = newDF.groupBy("email").agg(concat_ws(",", collect_set("url")) as "pageString")
-
-    val savedFilePath = "s3://eecs-practice/spark_test/test_fin"
     
-    reducedDF.rdd.repartition(1).saveAsTextFile(savedFilePath)
+    //look into checkpoint/ caching at this point so you don't recalculate the data frame each time
 
+    val savedFilePath = "s3://commoncrawltake2/ic" 
+
+    reducedDF.rdd.repartition(8).saveAsTextFile(savedFilePath)
     spark.stop()
-      }
-
-
-val analyze2: (WARCRecord => ArrayBuffer[Tuple2[String, String]]) = (record: WARCRecord) => {
-
-    var emails = ArrayBuffer[String]()
-    val content = record.getContent
-
-    var sizeread = 0
-    var word = ""
-    var startingSearch = true
-    for (b <- content){
-      var bc = b.toChar
-      if (startingSearch){
-        //if we are at the start of a word
-        if (!bc.isWhitespace){
-          sizeread +=1
-          word += bc
-          startingSearch = false
-        }
-
-      }
-      //if current search longer than email look for next boundary
-      else if (sizeread >= 200){
-        //check if starting word
-        if(bc.isWhitespace){
-          word = ""
-          sizeread = 0
-        }
-      }
-      //if at pontential start of word
-      else if (sizeread == 0){
-        //if at start of word
-        if(!bc.isWhitespace){
-          sizeread +=1
-          word +=bc
-        }
-      }
-      //if in middle of word
-      else{
-        //if another character
-        if(!bc.isWhitespace){
-          sizeread +=1
-          word += bc
-        }
-        //else if a boundary
-        else{
-          sizeread = 0
-          if(word.endsWith(".ic.gov") && word.contains("@")) emails += word
-          word = ""
-        }
-      }
-    }
-
-    if (emails.isEmpty) {
-      ArrayBuffer(("null", null))
-    } else {
-      val uri = new URI(record.getHeader.getTargetURI)
-      val url = uri.toURL.getHost()
-      for (email <- emails) yield {
-       (email, url.toString)
-      }
-    }
   }
 
 
+
+  def analyze(record: WARCRecord): Array[Tuple2[String, String]] = {
+
+    // TODO: can we make this statically defined or global so we don't have to instantiate a new one every time
+    val milEmailPattern = new Regex("""\b[A-Za-z0-9._%+-]{1,64}@.ic.gov\b""")
+
+    val content = new String(record.getContent)
+
+    val emails = milEmailPattern.findAllMatchIn(content).toArray.map(email => email.toString)
+    var final_array:Array[(String,String)]= Array()
+    
+    if (emails.isEmpty) {
+      return Array(("null", null))
+    } else {
+      val uri = new URI(record.getHeader.getTargetURI)
+      url = uri.toURL.getHost().toString
+      for (email <- emails)  yield {
+        (email, url.toString)
+      }
+      }
 }
